6510fabc9563128e39bb7a712b910018f33db97d
LeoPradoG <108141253+LeoPradoG@users.noreply.github.com>
2022-10-05 08:16:19 -0300

    Ajuste de formato de data

--- funcoes/sesmt/ajax_exibe_alert_durabilidade.php
+++ funcoes/sesmt/ajax_exibe_alert_durabilidade.php
@@ -6,39 +6,46 @@
     $var_usu_alert = $_GET['cd_usuario'];
 
     $cons_alert_prod= "SELECT 
-                            CASE 
-                            WHEN TO_CHAR(SYSDATE,'DD/MM/YYYY') 
-                            BETWEEN (SELECT TO_CHAR(MAX(sol.HR_CADASTRO),'DD/MM/YYYY') AS ULT_SOL
-                                    FROM portal_sesmt.SOLICITACAO sol
-                                    WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
-                                    AND sol.CD_USUARIO_MV = '$var_usu_alert') 
-                            AND TO_CHAR((SELECT MAX(sol.HR_CADASTRO)AS ULT_SOL
-                                        FROM portal_sesmt.SOLICITACAO sol
-                                        WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
-                                        AND sol.CD_USUARIO_MV = '$var_usu_alert')
-                                        + 
-                                        (SELECT lgdur.DIAS
-                                            FROM portal_sesmt.LOG_DURABILIDADE lgdur
-                                            WHERE lgdur.CD_DURABILIDADE IN (SELECT so.CD_DURABILIDADE
-                                                                            FROM portal_sesmt.SOLICITACAO so
-                                                                            WHERE so.CD_SOLICITACAO IN (SELECT MAX(sol.CD_SOLICITACAO)AS ULT_SOL
-                                                                                                    FROM portal_sesmt.SOLICITACAO sol
-                                                                                                    WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
-                                                                                                    AND sol.CD_USUARIO_MV = '$var_usu_alert'))
-                                            UNION ALL
-
-                                            SELECT dur.DIAS
-                                            FROM portal_sesmt.DURABILIDADE dur
-                                            WHERE dur.CD_DURABILIDADE IN (SELECT so.CD_DURABILIDADE
-                                                                        FROM portal_sesmt.SOLICITACAO so
-                                                                        WHERE so.CD_SOLICITACAO IN (SELECT MAX(sol.CD_SOLICITACAO)AS ULT_SOL
-                                                                                                    FROM portal_sesmt.SOLICITACAO sol
-                                                                                                    WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
-                                                                                                    AND sol.CD_USUARIO_MV = '$var_usu_alert'))),'DD/MM/YYYY')
-                            THEN 1
-                            ELSE 0
-                            END SN_ALERTA_DUR
-                            FROM DUAL";
+                       CASE
+                       WHEN TRUNC(TO_DATE(res.ATUAL,'DD/MM/YYYY'))
+                       BETWEEN TRUNC(TO_DATE(res.INICIO,'DD/MM/YYYY'))
+                       AND TRUNC(TO_DATE(res.FIM,'DD/MM/YYYY')) THEN 1
+                       ELSE 0
+                       END SN_ALERTA_DUR
+                       FROM(
+                       SELECT
+                       TO_CHAR(SYSDATE, 'DD/MM/YYYY') AS ATUAL,
+                       (SELECT TO_CHAR(MAX(sol.HR_CADASTRO), 'DD/MM/YYYY') AS ULT_SOL
+                       FROM portal_sesmt.SOLICITACAO sol
+                       WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
+                       AND sol.CD_USUARIO_MV = '$var_usu_alert') AS INICIO,
+                       TO_CHAR((SELECT MAX(sol.HR_CADASTRO) AS ULT_SOL
+                               FROM portal_sesmt.SOLICITACAO sol
+                               WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
+                               AND sol.CD_USUARIO_MV = '$var_usu_alert') +
+                               (SELECT lgdur.DIAS
+                               FROM portal_sesmt.LOG_DURABILIDADE lgdur
+                               WHERE lgdur.CD_DURABILIDADE IN
+                                   (SELECT so.CD_DURABILIDADE
+                                       FROM portal_sesmt.SOLICITACAO so
+                                       WHERE so.CD_SOLICITACAO IN
+                                           (SELECT MAX(sol.CD_SOLICITACAO) AS ULT_SOL
+                                               FROM portal_sesmt.SOLICITACAO sol
+                                               WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
+                                               AND sol.CD_USUARIO_MV = '$var_usu_alert'))
+                           UNION ALL
+                           SELECT dur.DIAS
+                               FROM portal_sesmt.DURABILIDADE dur
+                               WHERE dur.CD_DURABILIDADE IN
+                                   (SELECT so.CD_DURABILIDADE
+                                       FROM portal_sesmt.SOLICITACAO so
+                                       WHERE so.CD_SOLICITACAO IN
+                                           (SELECT MAX(sol.CD_SOLICITACAO) AS ULT_SOL
+                                               FROM portal_sesmt.SOLICITACAO sol
+                                               WHERE sol.CD_PRODUTO_MV = $var_prodto_alert
+                                               AND sol.CD_USUARIO_MV = '$var_usu_alert'))),
+                           'DD/MM/YYYY') AS FIM                
+                       FROM DUAL) res";
 
     $rest_alert_prod = oci_parse($conn_ora, $cons_alert_prod);
 
