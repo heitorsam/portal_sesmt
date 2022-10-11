da16a637f2d891ae8e703f6914ed4881324fa365
santacasasjc <scalabrinih@gmail.com>
2022-10-11 12:55:43 -0300

    check_leito

--- funcoes/ajax_editar_campos.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    include "../conexao.php";
-    
-    echo $update_final = $_POST["update_final"];
-    $result_editar= oci_parse($conn_ora, $update_final);
-    $valida_editar = oci_execute($result_editar);
-
-
-?>
